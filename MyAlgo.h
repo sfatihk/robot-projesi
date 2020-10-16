377d9251362724fa02d14b36c38c2b4c3e871ce5
sfatihk <sakkarmen@gmail.com>
2020-10-16 23:29:34 +0300

    Refactor algorithms

--- MyAlgo.h
+++ MyAlgo.h
@@ -1,11 +1,11 @@
 #ifndef MYALGO_H
 #define MYALGO_H
 #include <math.h>
-extern "C" __declspec(dllexport) bool Algo1(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo2(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo3(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo4(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo5(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo6(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
-extern "C" __declspec(dllexport) bool Algo7(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm1(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm2(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm3(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm4(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm5(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm6(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
+extern "C" __declspec(dllexport) bool algorithm7(unsigned short S, unsigned short L, unsigned short R, int Init, int * VL, int * VR);
 #endif // MYALGO_H
